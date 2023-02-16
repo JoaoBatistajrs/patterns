e220e89e65eb7ec03fe2343b3a768773e33d6a7d
João Batista Santos Junior <joao.santos@programmers.com.br>
2023-02-16 14:32:25 -0300

    First Commit

---
+++ src/test/java/patterns/gof/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package patterns.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
